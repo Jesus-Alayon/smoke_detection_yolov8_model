4ff8e01142584a4bdd079a983c5cb109e2d4581a
Jesús Baltazar González Alayón <73578220+Jesus-Alayon@users.noreply.github.com>
2023-12-02 04:47:40 -0600

    Add files via upload

---
+++ ROS_folder/drone_ws/build/rmtt_tracker/CMakeFiles/rmtt_tracker_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/jesus-alayon/drone_ws/devel/include/rmtt_tracker/tracker_pidConfig.h"
+  "/home/jesus-alayon/drone_ws/devel/lib/python3/dist-packages/rmtt_tracker/cfg/tracker_pidConfig.py"
+  "/home/jesus-alayon/drone_ws/devel/share/rmtt_tracker/docs/tracker_pidConfig-usage.dox"
+  "/home/jesus-alayon/drone_ws/devel/share/rmtt_tracker/docs/tracker_pidConfig.dox"
+  "/home/jesus-alayon/drone_ws/devel/share/rmtt_tracker/docs/tracker_pidConfig.wikidoc"
+  "CMakeFiles/rmtt_tracker_gencfg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/rmtt_tracker_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ROS_folder/drone_ws/build/rmtt_tracker/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jesus-alayon/drone_ws/src/rmtt_tracker
+# Build directory: /home/jesus-alayon/drone_ws/build/rmtt_tracker
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
