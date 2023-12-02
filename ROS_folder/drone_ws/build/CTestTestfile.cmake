8c46cc05cadd17a08ac634fa77de80d787b82eda
Jesús Baltazar González Alayón <73578220+Jesus-Alayon@users.noreply.github.com>
2023-12-02 04:41:32 -0600

    Add files via upload

---
+++ ROS_folder/drone_ws/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/jesus-alayon/drone_ws/src
+# Build directory: /home/jesus-alayon/drone_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rmtt_description")
+subdirs("rmtt_ros")
+subdirs("rmtt_apriltag")
+subdirs("rmtt_driver")
+subdirs("rmtt_teleop")
+subdirs("rmtt_tracker")
